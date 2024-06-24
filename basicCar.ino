7477ff2bc3fe5768ea8cac42881b172796fdd498
Hazem7Mohammad <77021386+Hazem7Mohammad@users.noreply.github.com>
2024-06-24 10:48:37 +0400

    Create basicCar.ino

---
+++ basicCar.ino
@@ -0,0 +1,97 @@
+
+
+
+int i=0;
+// -------------- H-bridge connection pins -------- 
+// Motor A
+ 
+int enA = 9;//pwm pin
+int in1 = 8;
+int in2 = 7;
+ 
+// Motor B
+ 
+int enB = 3;//pwm pin
+int in3 = 5;
+int in4 = 4;
+
+int speed = 120;// PWM from 0 to 255 
+
+void setup() {
+
+  Serial.begin(9600);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+}
+
+void loop() {
+
+
+delay(10000);//count 10sec before starting
+
+
+forward();
+delay(2000);
+off();
+right();
+delay(3000);
+off();
+delay(1000);
+left();
+delay(3000);
+off();
+delay(500);
+backward();
+delay(2000);
+off();
+}
+
+
+//-------------------H-bridge-------------------------------
+// car movement functions: enabling required motors with appropriate speeds
+void backward(){
+  analogWrite(enA, speed);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enB, speed);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+  
+}
+void forward(){
+  analogWrite(enB, speed);
+  analogWrite(enA, speed);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW); 
+}
+void right(){
+  
+  analogWrite(enA, speed);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW); 
+  analogWrite(enB, speed); 
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+void left(){
+  
+  analogWrite(enA, speed);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enB, speed);  
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+void off(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);  
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
