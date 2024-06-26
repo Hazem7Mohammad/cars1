b0019ce0096ef918ada61f932c672ce812666852
Hazem7Mohammad <77021386+Hazem7Mohammad@users.noreply.github.com>
2024-06-26 09:20:00 +0400

    Create flysky.ino

---
+++ Mode3/flysky.ino
@@ -0,0 +1,4 @@
+  ch1Value = readChannel(CH1, -100, 100, 0);
+  ch2Value = readChannel(CH2, -100, 100, 0);
+  ch3Value = readChannel(CH3, -100, 100, -100);
+  ch4Value = readChannel(CH4, -100, 100, 0);
