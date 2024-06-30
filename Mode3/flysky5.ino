9f7dff2ec120d53bf721f897aafcaa413117a8bd
Hazem7Mohammad <77021386+Hazem7Mohammad@users.noreply.github.com>
2024-07-01 00:02:04 +0400

    Create flysky5.ino

---
+++ Mode3/flysky5.ino
@@ -0,0 +1,149 @@
+#define CH1 13
+#define CH2 12
+#define CH3 11
+#define CH4 10
+
+int ch1Value;
+int ch2Value;
+int ch3Value;
+int ch4Value;
+
+int readChannel(int channelInput, int minLimit, int maxLimit, int defaultValue){
+  int ch = pulseIn(channelInput, HIGH, 30000);
+  if (ch < 100) return defaultValue;
+  return map(ch, 1000, 2000, minLimit, maxLimit);
+}
+
+int speed = 170;
+int turnSpeed = 200;
+int mapspeed;
+int mapspeed2;
+
+int enA = 9;
+int in1 = 8;
+int in2 = 7;
+
+int enB = 3;
+int in3 = 5;
+int in4 = 4;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(enA, OUTPUT);
+  pinMode(enB, OUTPUT);
+  pinMode(in1, OUTPUT);
+  pinMode(in2, OUTPUT);
+  pinMode(in3, OUTPUT);
+  pinMode(in4, OUTPUT);
+
+  Serial.begin(115200);
+  pinMode(CH1, INPUT);
+  pinMode(CH2, INPUT);
+  pinMode(CH3, INPUT);
+  pinMode(CH4, INPUT);
+}
+
+void loop() {
+  ch1Value = readChannel(CH1, -100, 100, 0);
+
+  ch2Value = readChannel(CH2, -100, 100, 0);
+  if (ch2Value > 5) {
+    mapspeed = map(ch2Value, 0, 100, 0, 255);
+    forward();
+    Serial.println(mapspeed);
+  } 
+  else if (ch2Value < -5) {
+    mapspeed = map(ch2Value, 0, -100, 0, 255);
+    backward();
+    Serial.println(mapspeed);
+  } 
+  else {
+    off();
+  }
+  ch3Value = readChannel(CH3, -100, 100, -100);
+  ch4Value = readChannel(CH4, -100, 100, 0);
+  if (ch4Value > 5) {
+    mapspeed2 = map(ch4Value, 0, 100, 0, 255);
+    right();
+    Serial.println(mapspeed2);
+  } 
+  else if (ch4Value < -5) {
+    mapspeed2 = map(ch4Value, 0, -100, 0, 255);
+    left();
+    Serial.println(mapspeed2);
+  } 
+  else {
+    off();
+  }
+
+
+  Serial.print("Ch1: ");
+  Serial.print(ch1Value);
+  Serial.print(" | Ch2: ");
+  Serial.print(ch2Value);
+  Serial.print(" | Ch3: ");
+  Serial.print(ch3Value);
+  Serial.print(" | Ch4: ");
+  Serial.println(ch4Value);
+
+  
+  
+  // if(ch1Value > 50){
+  //   forward();
+  // }
+  // else if(ch1Value < -50){
+  //   backward();
+  // }
+  // else if(ch2Value > 50){
+  //   right();
+  // }
+  // else if(ch2Value < -50){
+  //   left();
+  // }
+  // else {
+  //   off();
+  // }
+}
+
+void backward(){
+  analogWrite(enA, mapspeed);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enB, mapspeed);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void forward(){
+  analogWrite(enB, mapspeed);
+  analogWrite(enA, mapspeed);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void left(){
+  analogWrite(enA, mapspeed2);
+  digitalWrite(in1, HIGH);
+  digitalWrite(in2, LOW);
+  analogWrite(enB, mapspeed2);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, HIGH);
+}
+
+void right(){
+  analogWrite(enA, mapspeed2);
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, HIGH);
+  analogWrite(enB, mapspeed2);
+  digitalWrite(in3, HIGH);
+  digitalWrite(in4, LOW);
+}
+
+void off(){
+  digitalWrite(in1, LOW);
+  digitalWrite(in2, LOW);
+  digitalWrite(in3, LOW);
+  digitalWrite(in4, LOW);
+}
